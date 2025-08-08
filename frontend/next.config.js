d863b06fd25eea32836e545aa30ca97051d0d4a9
sidharthnayyar89 <tagloose@gmail.com>
2025-08-08 14:20:22 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,11 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: "/api/:path*", destination: "https://amazons-ads-backend.onrender.com/api/:path*" },
+      { source: "/health", destination: "https://amazons-ads-backend.onrender.com/health" },
+    ];
+  },
+};
+module.exports = nextConfig;
